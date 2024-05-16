4398b4b819c386b62695030b011496ca23a04461
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2024-05-16 20:54:56 -0300

    update 16052024

--- src/main/java/br/com/deploy/render/controller/LivrosController.java
+++ src/main/java/br/com/deploy/render/controller/LivrosController.java
@@ -2,13 +2,21 @@ package br.com.deploy.render.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 // Controlador para lidar com as visualizações
 @Controller
 public class LivrosController {
     @GetMapping("/")
     public String home() {
-        return "index"; // Certifique-se de que há um arquivo index.html na pasta templates
+        return "index";
     }
+
+    @GetMapping("/index")
+    public String getMethodName(@RequestParam String param) {
+        return "index";
+    }
+    
 }
 
--- src/main/java/br/com/deploy/render/controller/LivrosRestController.java
+++ src/main/java/br/com/deploy/render/controller/LivrosRestController.java
@@ -2,7 +2,9 @@ package br.com.deploy.render.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.deploy.render.model.Livro;
 import br.com.deploy.render.repository.LivrosRepository;
@@ -21,8 +23,10 @@ public class LivrosRestController {
         return (List<Livro>) this.livrosRepository.findAll();
     }
 
-    @PostMapping("/livros")
-    public Livro create(@RequestBody Livro livro) {
-        return this.livrosRepository.save(livro);
+    @PostMapping("/novoLivro")
+    public ModelAndView create(@RequestBody Livro livro) {
+        ModelAndView modelAndView = new ModelAndView("index");
+        livrosRepository.save(livro);
+        return modelAndView;
     }
 }
