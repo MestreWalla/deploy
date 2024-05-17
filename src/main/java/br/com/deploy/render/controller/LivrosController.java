3d22594bd5fd47ba10d69e1faf3c0ac8818ac740
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2024-05-16 21:11:00 -0300

    update

--- src/main/java/br/com/deploy/render/controller/LivrosController.java
+++ src/main/java/br/com/deploy/render/controller/LivrosController.java
@@ -1,13 +1,22 @@
 package br.com.deploy.render.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import br.com.deploy.render.repository.LivrosRepository;
 
+import br.com.deploy.render.model.Livro;
 
 // Controlador para lidar com as visualizações
 @Controller
 public class LivrosController {
+    @Autowired
+    LivrosRepository livrosRepository;
+
     @GetMapping("/")
     public String home() {
         return "index";
@@ -17,6 +26,12 @@ public class LivrosController {
     public String getMethodName(@RequestParam String param) {
         return "index";
     }
-    
-}
 
+    @PostMapping("/novoLivro")
+    public ModelAndView create(@RequestBody Livro livro) {
+        ModelAndView modelAndView = new ModelAndView("index");
+        livrosRepository.save(livro);
+        return modelAndView;
+    }
+
+}
--- src/main/java/br/com/deploy/render/controller/LivrosRestController.java
+++ src/main/java/br/com/deploy/render/controller/LivrosRestController.java
@@ -2,15 +2,10 @@ package br.com.deploy.render.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
 import br.com.deploy.render.model.Livro;
 import br.com.deploy.render.repository.LivrosRepository;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 // Controlador para lidar com as APIs REST
 @RestController
@@ -21,12 +16,5 @@ public class LivrosRestController {
     @GetMapping("/livros")
     public List<Livro> list() {
         return (List<Livro>) this.livrosRepository.findAll();
-    }
-
-    @PostMapping("/novoLivro")
-    public ModelAndView create(@RequestBody Livro livro) {
-        ModelAndView modelAndView = new ModelAndView("index");
-        livrosRepository.save(livro);
-        return modelAndView;
     }
 }
