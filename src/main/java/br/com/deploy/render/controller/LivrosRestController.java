7b1a52733983d7c5aad6f2e0be64e28444174ae0
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2024-05-16 22:27:49 -0300

    update src/main/java/br/com/deploy/render/controller/LivrosRestController.java

--- src/main/java/br/com/deploy/render/controller/LivrosRestController.java
+++ src/main/java/br/com/deploy/render/controller/LivrosRestController.java
@@ -3,9 +3,11 @@ package br.com.deploy.render.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import br.com.deploy.render.model.Livro;
 import br.com.deploy.render.repository.LivrosRepository;
-import org.springframework.web.bind.annotation.GetMapping;
 
 // Controlador para lidar com as APIs REST
 @RestController
@@ -16,5 +18,10 @@ public class LivrosRestController {
     @GetMapping("/livros")
     public List<Livro> list() {
         return (List<Livro>) this.livrosRepository.findAll();
+    }
+
+    @PostMapping("/novoLivro")
+    public Livro addLivro(@RequestBody Livro livro) {
+        return this.livrosRepository.save(livro);
     }
 }
